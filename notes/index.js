72772ae2fa5ef784e64b95344ea5f61fc5e6a274
vigneshwaran <vigneshwaran.ravi@makoitlab.com>
2025-04-28 15:22:22 +0530

    updated region and added new console

--- notes/index.js
+++ notes/index.js
@@ -33,6 +33,7 @@ const docClient = DynamoDBDocumentClient.from(dynamoClient, translateConfig);
 export const handler = async (event, context, callback) => {
   console.log("event: ", event);
   console.log("hello world: ", event);
+  console.log("hello world: ", event);
   const done = (err, res) =>
     callback(null, {
       statusCode: err ? "400" : "200",
